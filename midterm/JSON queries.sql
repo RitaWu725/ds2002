c4a879dda5fcd2566be842348a623d27291ad79f
RitaWu725 <124289313+RitaWu725@users.noreply.github.com>
2023-03-12 16:56:43 -0400

    Delete JSON queries.sql

--- midterm/JSON queries.sql
+++
@@ -1,6 +0,0 @@
-use sakila_dw;
-select * from sakila_dw.dim_customer;
-select * from sakila_dw.dim_staff;
-select * from sakila_dw.dim_store;
-select * from sakila_dw.dim_rental;
-select * from sakila_dw.dim_date;
